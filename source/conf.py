bdc8d1d918e2ec8c772c14c20e96e89901d86c58
Jacek Burzynski <jburzyn@gmail.com>
2017-07-18 17:40:53 +0200

    version update

--- source/conf.py
+++ source/conf.py
@@ -57,7 +57,7 @@ author = u'Jacek Burzynski'
 # built documents.
 #
 # The short X.Y version.
-version = u'1.5.5'
+version = u'1.5.6'
 # The full version, including alpha/beta/rc tags.
 release = version
 
